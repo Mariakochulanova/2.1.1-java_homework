2ff73b3bd7b3f6bad543c9f33b47fb903a71e11b
Maria Kochulanova <holamarymar@gmail.com>
2020-08-18 12:38:36 +0300

    Application for Miles created

---
+++ src/Miles.java
@@ -0,0 +1,13 @@
+public class Miles {
+    public static void main(String[] args) {
+        int mile = 20;
+        int ticketprice;
+        ticketprice = 20_000;
+        int totalmiles = ticketprice / mile;
+
+        System.out.println(totalmiles);
+
+
+    }
+}
+
